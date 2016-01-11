25ecfe32904ff0e3352024c9100b3f7ec885e5a8
Masashi Sato <ms2.sato@gmail.com>
2016-01-11 07:11:39 +0000

    form

--- app.rb
+++ app.rb
@@ -5,3 +5,12 @@ get '/' do
     @now = Time.now
     erb :index
 end
+
+get '/contact_new' do
+    erb :contact_form
+end
+
+post '/contacts' do
+    @name = params[:name]
+    erb :contact_confirm
+end
